8eab4c174fa25c15fe83f715603062f9a8baaba9
sanyam810 <sanyamsaini081@gmail.com>
2023-03-17 02:16:25 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,34 +7,49 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`
-      ${styles.paddingX} absolute inset-0
+      ${styles.padding} absolute inset-0
       top-[120px] max-w-7xl mx-auto flex 
       flex-row items-start gap-5`}
       >
         <div className="flex flex-col
-        justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
-          <div className="w-1 sm:h-80 h-40 violet-gradient"/>
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#e5ec2c]">
-            Sanyam</span>
+        justify-center items-center mt-5 ml-56">
+           {/* <div className="w-5 h-5 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"/>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"/>  */}
+        </div> 
+        <div className="flex flex-col justify-center items-center">
+          <h1 className={`${styles.heroHeadText} font-extrabold  text-white`}>
+              Food<span 
+                className={`${styles.heroHeadText} font-extrabold text-transparent 
+                bg-clip-text bg-gradient-to-r
+              from-purple-600 via-pink-600 to-blue-600`}>
+                  Ful</span>
+           {/* <span className="text-[#e5ec2c]">
+            Sanyam
+            </span> */}
           </h1>
-          <p className={`${styles.heroSubText}  mt-2 text-white-100`}>
-            I love to create animations<br className="sm:block hidden"/>
-            and currently learning and exploring the world of programming.
-          </p>
+          
+            
+          <h1 className={`${styles.heroSubText}  mt-4 text-white-100 `}>
+            Zero Hunger. Zero Poverty. Sustainable Cities.<br className="sm:block hidden"/>
+            
+          </h1>
+          
+    
+          
         </div>
-
+        
+        
+         
         
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas className="mt-10"/>
+      
       
       <div className="absolute xs:bottom-10 bottom-32
        w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl
-           border-4 border-secondary
+           border-4 border-violet-gradient
            flex justify-center items-start p-2">
             <motion.dev
               animate={{
@@ -45,7 +60,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: 'loop'
               }}
-              className="w-3 h-3 rounded-full  bg-secondary mb-1"
+              className="w-3 h-3 rounded-full  bg-white"
             />
           </div>
         </a>
